d0fd8dd6be03bd1c60ab4da30e79947173154c21
David Chen <45975185+david162929@users.noreply.github.com>
2019-02-18 01:15:33 +0800

    create web page for API

--- app.js
+++ app.js
@@ -1,9 +1,37 @@
 const express = require("express");
+const bodyParser = require("body-parser");
+const cookieParser = require("cookie-parser");
+
 const app = express();
 
+//use bodyParser and cookieParser
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+
+//set pug
+app.set("view engine", "pug");
+
+
 app.get("/",(req, res) => {
-	res.send("Hello!!!");
+	res.render("index.pug");
+});
+
+app.get("/admin/product.html",(req, res) => {
+	res.render("product.pug");
+});
+
+app.get("/admin/campaign.html",(req, res) => {
+	res.render("campaign");
+});
+
+app.get("/admin/checkout.html",(req, res) => {
+	res.render("checkout");
 });
 
 
-app.listen(3000);
+
+
+
+app.listen(3000, () => {
+	console.log("this app is running on port 3000.");
+});
