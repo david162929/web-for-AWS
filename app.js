78ce96911fe05a3f08f03b29edfce9d4d8812019
David Chen <45975185+david162929@users.noreply.github.com>
2019-02-18 14:08:23 +0800

    add module mysql

--- app.js
+++ app.js
@@ -11,7 +11,32 @@ app.use(cookieParser());
 //set pug
 app.set("view engine", "pug");
 
+//mysql module
+var mysql = require('mysql');
 
+var con = mysql.createConnection({
+  host: "52.15.89.192",
+  user: "ec2-user",
+  password: "123qweasdzxc",
+  database: "assignment"
+});
+
+con.connect(function(err) {
+  if (err) throw err;
+  console.log("Connected!");
+
+/*   con.query("SELECT * FROM user", function (err, result, fields) {
+    if (err) throw err;
+	console.log(result);
+  });  */
+    
+});
+
+
+
+
+
+//Route
 app.get("/",(req, res) => {
 	res.render("index.pug");
 });
@@ -19,6 +44,27 @@ app.get("/",(req, res) => {
 app.get("/admin/product.html",(req, res) => {
 	res.render("product.pug");
 });
+
+//Poduct API 1.0
+app.post("/api/1.0/admin/product", (req, res) => {
+	const id = req.body.id;
+	const title = req.body.title;
+	const description = req.body.description;
+	const price = req.body.price;
+	const texture = req.body.texture;
+	const wash = req.body.wash;
+	const place = req.body.place;
+	const note = req.body.note;
+	const story = req.body.story;
+	
+	
+	console.log(id, title, description, price, texture, wash, place, note, story);
+	res.redirect("/admin/product.html");
+	
+});
+
+
+
 
 app.get("/admin/campaign.html",(req, res) => {
 	res.render("campaign");
@@ -31,7 +77,7 @@ app.get("/admin/checkout.html",(req, res) => {
 
 
 
-
+//Port
 app.listen(3000, () => {
 	console.log("this app is running on port 3000.");
 });
